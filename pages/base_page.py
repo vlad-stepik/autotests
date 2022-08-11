b9de69b81ad3351e80b6aa060d284a2a55db770b
VladKomlyukov <vladkomlyukov@gmail.com>
2022-08-11 18:23:38 +0300

    Add new method for exceptions

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,20 @@
-
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     # Т.к. изначально создали: self.browser = browser и self.url = url,
     # то в своей функции обращаемся также к self.browser и self.url
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
--- pages/main_page.py
+++ pages/main_page.py
@@ -4,3 +4,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+    def should_be_login_link(self):
+       assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
