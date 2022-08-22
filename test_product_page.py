acdc635a2f73948349273daa643a4cd370a43913
VladKomlyukov <vladkomlyukov@gmail.com>
2022-08-22 13:14:16 +0300

    Add updates for code-review

--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,7 @@ reg_url = 'http://selenium1py.pythonanywhere.com/accounts/login/'
 
 
 # тест для проверки добавления товара в корзину
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
@@ -55,12 +56,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 @pytest.mark.smoke
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, base_url)
     page.open()
@@ -84,6 +87,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
